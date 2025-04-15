5fd98f6822f89961426c2aa19f0f80cd42e87b6c
Merey <yerbm01@pfw.edu>
2025-04-15 15:33:02 -0400

    Cypress: User Login

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:8000',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true,
+  },
+} 
